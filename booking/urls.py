5b1f41322c75be69ead0c9b5b6a084d731a2f2c0
Denis Salnikov <denissalnikov@ukr.net>
2025-06-11 19:41:01 +0300

    Added base template
    Added location list page.
    Added location detail page.
    Added booking form at location detail page.

--- booking/urls.py
+++ booking/urls.py
@@ -4,5 +4,7 @@ from booking import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('location-list/', views.location_list, name='locations'),
+    path('location-detail/<int:location_id>', views.location_detail, name='location_detail'),  # 127.0.0.1/location-detail/2
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Location, Booking
 
 
 # Create your views here.
 def home(request):
     return render(request, "booking/home.html")
+
+
+def location_list(request):
+    loc_list = Location.objects.filter(is_active=True)
+    return render(request, "booking/locations.html", context={"locations": loc_list})
+
+
+@login_required
+def location_detail(request, location_id):
+    location = get_object_or_404(Location, pk=location_id)
+    error = ''
+    if request.method == "POST":
+        try:
+            booking = Booking.objects.create(
+                user=request.user,
+                location=location,
+                start_date=request.POST.get('start_date'),
+                end_date=request.POST.get('end_date')
+            )
+            return redirect('main:home')
+        except ValidationError as exp:
+            error = exp.message
+    return render(request, "booking/location_detail.html", context={"location": location,
+                                                                    "error": error})
